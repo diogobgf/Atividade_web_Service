4e709961592b62a9c64fe887878c296c1802a2d4
lumabarbosa <lumarrbarbosa@gmail.com>
2023-12-07 10:31:08 -0300

    web_ser

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo ao meu web service!');
+  });
+
+const porta = 3000;
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`);
+  });
